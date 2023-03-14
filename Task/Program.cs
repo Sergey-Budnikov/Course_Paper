bc6e64a94205642934e021ef6c4eab9dceeaab32
Будников Сергей <mr.s.b@bk.ru>
2023-03-14 13:29:09 +0300

    Условие задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] array = { "Chick", "Pshik", "Geek", ":-)", "52" };
+
+void Method (string[] arr)
+{
+for (int i = 0; i < array.Count(); i++)
+{
+    if (array[i].Length <= 3)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+}
+Method(array);
+
+
+// Вариант №2. Однокурсник Ревякин Илья.
+// string [] Result = {""};
+
+//  while (true)
+//  {
+//     Console.WriteLine("Введите 0000 если хотите закончить ввод");
+//      string Word = Console.ReadLine();
+//      if (Word != "0000") LongWordThree(Word, ref Result);
+//      else {ShowArray(Result); return;}
+
+// }
+
+// void LongWordThree(string word,ref string[] Result )
+// {
+//     if (word.Length < 4) AppEnd(ref Result, word);
+// }
+
+// void AppEnd(ref string[] Result,string word)
+// {
+//     string[] Res = new string[Result.Length+1];
+//     for (int i = 0; i < Result.Length; i++)
+//     {
+//         Res[i] = Result[i];
+//     }
+//     Res[Res.Length-1] = word;
+//     Result = Res;
+// }
+
+// void ShowArray(string[] testArray)
+// {
+//     string[] showArray = testArray;
+//     var str = string.Join(" ", showArray);
+//     Console.WriteLine(str);
+// }
+
+
+
+
